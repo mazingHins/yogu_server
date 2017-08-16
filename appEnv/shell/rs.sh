5858b79e3b98438683673bc3a6b2428c45c556c8
hins <qiujun@ibeiliao.com>
2017-08-16 20:41:48 +0800

    修改resin环境

--- appEnv/shell/rs.sh
+++ appEnv/shell/rs.sh
@@ -124,7 +124,7 @@ slbEcsEnable() {
 stopServer() {
     echo 'stop resin......'
     echo "conf=$CONF, server=$SERVER"
-    /usr/local/resin/bin/resin.sh -conf $CONF -log-directory $LOG_PATH -root-directory /usr/local/resin -server $SERVER shutdown 
+    /usr/local/resin/resin-pro-4.0.52/bin/resin.sh -conf $CONF -log-directory $LOG_PATH -root-directory /usr/local/resin/resin-pro-4.0.52 -server $SERVER shutdown 
 }
 
 
@@ -143,7 +143,7 @@ startServer() {
 
     echo 'start resin......'
     echo "conf=$CONF, server=$SERVER"
-    /usr/local/resin/bin/resin.sh -conf $CONF -log-directory $LOG_PATH -root-directory /usr/local/resin -server $SERVER start 
+    /usr/local/resin/resin-pro-4.0.52/bin/resin.sh -conf $CONF -log-directory $LOG_PATH -root-directory /usr/local/resin/resin-pro-4.0.52 -server $SERVER start 
 }
 
 
