d1e7c1302b0ce76bcc45e6fc6fa5a356ef510b3a
qiu.jun <qiu.jun@dtyunxi.com>
2017-10-28 11:04:40 +0800

    1

---
+++ web/backend-admin/src/main/java/com/yogu/services/backend/admin/resources/order/OrderManagementResource.java
@@ -0,0 +1,15 @@
+package com.yogu.services.backend.admin.resources.order;
+
+import org.springframework.stereotype.Controller;
+
+import com.yogu.commons.utils.resource.Menu;
+
+
+/**
+ * 订单管理，这里仅用于定义菜单
+ * @author ten 2015/9/25
+ */
+@Controller
+@Menu(name = "订单管理", parent = "", sequence = 300000)
+public class OrderManagementResource {
+}
