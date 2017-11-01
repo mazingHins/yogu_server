6e88f4c8ae32f5fead60549ac36af5e6d52703f0
qiu.jun <qiu.jun@dtyunxi.com>
2017-11-01 17:31:03 +0800

    1

--- services/xcommon/src/main/java/com/yogu/commons/sdk/user/MazingGetEncryptKeyThread.java
+++ services/xcommon/src/main/java/com/yogu/commons/sdk/user/MazingGetEncryptKeyThread.java
@@ -1,32 +1,13 @@
 package com.yogu.commons.sdk.user;
 
-import com.yogu.CommonConstants;
-import com.yogu.commons.sdk.user.encrypt.ConfigurationDecoder;
-import com.yogu.commons.utils.JsonUtils;
-import com.yogu.commons.utils.encrypt.AESUtil;
-import com.yogu.commons.utils.encrypt.HMacSHA1;
+import java.util.HashMap;
+import java.util.Map;
 
-import org.apache.commons.lang3.StringUtils;
-import org.apache.http.HttpEntity;
-import org.apache.http.client.config.RequestConfig;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.ssl.SSLContextBuilder;
-import org.apache.http.ssl.TrustStrategy;
-import org.apache.http.util.EntityUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.json.JsonObject;
-import javax.net.ssl.SSLContext;
-
-import java.security.cert.CertificateException;
-import java.security.cert.X509Certificate;
-import java.util.HashMap;
-import java.util.Map;
+import com.yogu.commons.sdk.user.encrypt.ConfigurationDecoder;
+import com.yogu.commons.utils.encrypt.AESUtil;
 
 /**
  * 定时从帐号系统获取米星的加密 key
@@ -52,6 +33,9 @@ public class MazingGetEncryptKeyThread extends Thread {
     private ConfigurationDecoder decoder;
 
     private MazingWebContext context;
+    
+ // cookie过期时间：14天 （单位: 秒）
+ 	private static final int DEFAULT_COOKIE_TIMEOUT = 14 * 24 * 3600;
 
     /**
      * 构造函数
@@ -145,85 +129,25 @@ public class MazingGetEncryptKeyThread extends Thread {
     }
 
     /**
-     * 创建要访问的URL，包括签名
-     * @return
-     */
-    private String createUrlAndSign() {
-        String tmpAppId = getAppKey();
-        String tmpAppKey = getAppSecret();
-        long t = System.currentTimeMillis();
-        String str = tmpAppId + t;
-		String sign = HMacSHA1.getSignature(str, tmpAppKey);
-		return new StringBuilder(CommonConstants.USER_DOMAIN).append("/api/security/getKey")//
-				.append("?akey=").append(tmpAppId).append("&t=").append(t).append("&sign=").append(sign).toString();
-	}
-
-    /**
      * 从帐号系统读取key
      * @return
      * @throws Exception
      */
     private Map<String, Object> readKey() throws Exception {
-        // CloseableHttpClient httpclient = HttpClients.createDefault();
-    	CloseableHttpClient httpclient = createSSLClientDefault();// jfan 2016-01-21
-
-        RequestConfig requestConfig = RequestConfig.custom().setConnectTimeout(UserSdkConstants.CONNECT_TIMEOUT)
-                .setSocketTimeout(UserSdkConstants.SO_TIMEOUT).build();
-        HttpGet httpGet = new HttpGet(createUrlAndSign());
-        httpGet.setConfig(requestConfig);
-        CloseableHttpResponse response = httpclient.execute(httpGet);
-
-        try {
-            HttpEntity entity = response.getEntity();
-
-            String value = EntityUtils.toString(entity);
-            if (StringUtils.isNotBlank(value)) {
-                JsonObject jo = JsonUtils.toJsonObject(value);
-                boolean success = jo.getBoolean("success", false);
-                if (success) {
-                    JsonObject obj = jo.getJsonObject("object");
-
-					String key1 = obj.getString("key1");
-					String key2 = obj.getString("key2");
-					long timeout = obj.getJsonNumber("timeout").longValue();
-					logger.info("mazing#sdk#login | 读取到key内容 | key1: {}, key2: {}, timeout: {}", key1, key2, timeout);
-
-                    Map<String, Object> map = new HashMap<>(4);
-                    if (key1 != null) {
-                        map.put("key1", AESUtil.decrypt(ENCRYPT_KEY, key1));
-                    }
-                    if (key2 != null) {
-                        map.put("key2", AESUtil.decrypt(ENCRYPT_KEY, key2));
-                    }
-                    map.put("timeout", timeout);
-                    return map;
-                }
-
-            }
-            logger.error("mazing#sdk#login | 读取key失败 | value: {}", value);
-        } finally {
-            response.close();
+    	
+    	String key1 = AESUtil.encrypt(ENCRYPT_KEY, "232561032u32uXf-8930kxbs-1ytemn");
+		String key2 = AESUtil.encrypt(ENCRYPT_KEY, "plskdki3223234-8930kxbs-xfd-32");
+		
+		Map<String, Object> map = new HashMap<>(4);
+        if (key1 != null) {
+            map.put("key1", AESUtil.decrypt(ENCRYPT_KEY, key1));
         }
-        return null;
+        if (key2 != null) {
+            map.put("key2", AESUtil.decrypt(ENCRYPT_KEY, key2));
+        }
+        map.put("timeout", DEFAULT_COOKIE_TIMEOUT);
+        return map;
     }
 
-	/**
-	 * 创建httpClient，可自动接受ssl
-	 */
-	private CloseableHttpClient createSSLClientDefault() {
-		try {
-			SSLContext sslContext = new SSLContextBuilder().loadTrustMaterial(null, new TrustStrategy() {
-				// 信任所有
-				public boolean isTrusted(X509Certificate[] chain, String authType) throws CertificateException {
-					return true;
-				}
-			}).build();
-			SSLConnectionSocketFactory sslsf = new SSLConnectionSocketFactory(sslContext);
-			return HttpClients.custom().setSSLSocketFactory(sslsf).build();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return HttpClients.createDefault();
-	}
 
 }
