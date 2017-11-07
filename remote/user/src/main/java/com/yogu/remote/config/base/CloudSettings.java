4b6d3de6cb7d27e98fc31dd4720ab5f15dbe14e6
qiu.jun <qiu.jun@dtyunxi.com>
2017-11-07 10:32:36 +0800

    3

--- remote/user/src/main/java/com/yogu/remote/config/base/CloudSettings.java
+++ remote/user/src/main/java/com/yogu/remote/config/base/CloudSettings.java
@@ -56,7 +56,7 @@ public class CloudSettings {
      * @return
      */
     public static String getFileStoreBucketPublicUrl(String id) {
-        String key = cloud + "_bucket_" + id + "_url";
+        String key = cloud + "_bucket_" + "_url";
         return ConfigRemoteService.getConfig(ConfigGroupConstants.CLOUD, key);
     }
 
